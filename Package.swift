08899823a31a127b79a5175a88deade6cda7fe13
Ryo Aoyama <r.fe51028.r@gmail.com>
2017-11-02 16:20:00 +0900

    Swift4

--- Package.swift
+++ Package.swift
@@ -1,8 +1,12 @@
+// swift-tools-version:4.0
 import PackageDescription
 
 let package = Package(
     name: "Alembic-Sample",
     dependencies: [
-        .Package(url: "https://github.com/ra1028/Alembic.git", majorVersion: 3),
+        .package(url: "https://github.com/ra1028/Alembic.git", .upToNextMinor(from: "3.1.0"))
+    ],
+    targets: [
+        .target(name: "Alembic-Sample", dependencies: ["Alembic"], path: "Sources")
     ]
 )

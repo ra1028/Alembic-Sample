fd48f6a94414f2ae0865e21e50caacc8a1c92eb3
Ryo Aoyama <r.fe51028.r@gmail.com>
2017-11-20 05:04:52 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "Alembic-Sample",
     dependencies: [
-        .package(url: "https://github.com/ra1028/Alembic.git", .upToNextMinor(from: "3.1.0"))
+        .package(url: "https://github.com/ra1028/Alembic.git", .upToNextMinor(from: "3.2.0"))
     ],
     targets: [
         .target(name: "Alembic-Sample", dependencies: ["Alembic"], path: "Sources")
